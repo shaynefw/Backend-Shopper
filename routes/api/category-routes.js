be857a06ff3d075499324ea3525b2ec82d319181
swmac <shaynefw@gmail.com>
2023-02-24 10:27:02 -0500

    added get and get id for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,18 +3,46 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }]
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    }); // req.params.id is the id of the category
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' }); // if no category is found, return
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new category
+
+  Category.create({
+    category_name: req.body.category_name
+  })
 });
 
 router.put('/:id', (req, res) => {
