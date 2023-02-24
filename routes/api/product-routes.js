ba6df2b0c74fc830aceeb315c54b4216c22b09fd
swmac <shaynefw@gmail.com>
2023-02-24 17:30:13 -0500

    added get post and delete to product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,32 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err, { message: 'Products not found!' });
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }],
+    });
+
+   !productData ? res.status(404).json({ message: 'No product found with this id!' }) : res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err, { message: 'Product not found!' });
+  }
 });
 
 // create new product
@@ -85,12 +102,23 @@ router.put('/:id', (req, res) => {
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
       // console.log(err);
-      res.status(400).json(err);
+      res.status(400).json(err, { message: 'Product not updated!' });
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    !productData ? res.status(404).json({ message: 'Something went wrong..' }) : res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err, { message: 'Product not deleted!' });
+  }
 });
 
 module.exports = router;
