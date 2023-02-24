030a760db202dfa3a1788fac0fdfe337a7675c01
swmac <shaynefw@gmail.com>
2023-02-24 17:06:11 -0500

    added post put and delete for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -13,7 +13,7 @@ router.get('/', async (req, res) => {
     });
     res.status(200).json(categoryData);
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err, { message: 'Categories not found!' });
   }
 });
 
@@ -33,24 +33,46 @@ router.get('/:id', async (req, res) => {
 
     res.status(200).json(categoryData);
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err, { message: 'Category not found!' });
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
-
-  Category.create({
-    category_name: req.body.category_name
-  })
+  try {
+    const categoryData = await Category.create(req.body); // req.body is the category name
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err, { message: 'Category creation failed!' });
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    !categoryData[0] ? res.status(404).json({ message: 'No category found with this id!' }) : res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err, { message: 'Category update failed!' });
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      }
+    });
+    !categoryData ? res.status(404).json({ message: 'No category found with this id!' }) : res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
